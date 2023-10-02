bdf4d7187c19f672baa6b7b56805361e52e9bde4
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-10-02 10:19:32 -0700

    task 11 fix

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-# importing sys module to use write function
 import sys
-
-#print to stderr
 sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-
-# exit with status code 1
 sys.exit(1)
