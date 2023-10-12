84e739de514d09a089711aa63fbd3983d29da696
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-10-12 01:50:17 -0700

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
 
     length = len(my_list)
 
@@ -8,6 +8,6 @@ def replace_in_list(my_list, idx, element):
         return my_list
     elif idx >= length:
         return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+
+    my_list[idx] = element
+    return my_list
