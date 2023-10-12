bf169b40b11dd435893ef997801e3a23a8dc4bca
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-10-12 01:54:04 -0700

    tak 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,5 +9,7 @@ def new_in_list(my_list, idx, element):
     elif idx >= length:
         return my_list
 
-    my_list[idx] = element
-    return my_list
+    new_list = my_list[:]
+
+    new_list[idx] = element
+    return new_list
