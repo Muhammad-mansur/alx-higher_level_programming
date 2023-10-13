531b03ae4b1914695a90cf169e9054dee84162dd
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-10-13 07:26:53 -0700

    8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,7 +5,7 @@ def multiple_returns(sentence):
     length = len(sentence)
 
     if length == 0:
-        return (None,)
+        return (0, None)
     else:
         first = sentence[0]
         return (length, first)
