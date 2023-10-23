14ee7f12837e686e319d94b3c2ea8b30ee27013f
Muhammad Mansur <muhammadmansur737@gmail.com>
2023-10-23 15:20:34 -0700

    space rm

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,8 +5,6 @@ def safe_function(fct, *args):
     rlt = None
     try:
         rlt = fct(*args)
-
     except Exception as err:
         sys.stderr.write("Exception: {}\n".format(err))
-
     return rlt
