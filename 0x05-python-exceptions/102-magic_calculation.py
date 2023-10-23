11c4a3b6dd8bc20a183db77da20f76342a369713
Muhammad Mansur <muhammadmansur737@gmail.com>
2023-10-23 16:44:16 -0700

    adv 3 fix

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,13 +3,13 @@
 def magic_calculation(a, b):
     rlt = 0
 
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception("Too far")
             rlt += (a ** b) / i
-        except:
-            rlt += b + a
+        except Exception:
+            rlt = b + a
             break
 
     return rlt
