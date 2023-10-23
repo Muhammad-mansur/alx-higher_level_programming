bc53df1a7ae8240522a482e1c5d9e6d7953da61c
Muhammad Mansur <muhammadmansur737@gmail.com>
2023-10-23 14:59:33 -0700

    adv 1 fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 def safe_print_integer_err(value):
 
     try:
@@ -7,5 +9,5 @@ def safe_print_integer_err(value):
         return True
 
     except Exception as e:
-        sys.stderr.write("Exception: " + str(e) + "\n"
+        sys.stderr.write("Exception: " + str(e) + "\n")
         return False
