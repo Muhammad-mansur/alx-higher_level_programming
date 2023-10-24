bd8b897365763022f7f14a1f535570e04696b2ff
Muhammad Mansur <muhammadmansur737@gmail.com>
2023-10-24 15:03:41 -0700

    added comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+""" Define square class """
 
 class Square:
+    """
+    square class
+
+    Attributes:
+        size: size of the square
+    """
 
     def __init__(self, size):
+        """
+        initialized
+        
+        Args:
+            size: size of the square
+        """
         self.__size = size
