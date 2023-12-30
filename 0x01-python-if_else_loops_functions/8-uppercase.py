fedec458154a4869d0195d87bc4b9caf667dfbd6
Muhammad Mansur <muhammadmansur737@gmail.com>
2023-12-30 02:56:14 -0800

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            c = chr(ord(char) - 32)
+        else:
+            c = char
+            print("{}".format(c), end="")
+    print()
