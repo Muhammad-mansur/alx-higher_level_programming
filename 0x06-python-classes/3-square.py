8b045fb1ad1d233afa5e33fd6265eaf78a014a92
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-01-03 12:06:35 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+A class square that defines a square with private instance and
+public instance method
+"""
+
+
+class Square:
+    """
+    a class square with prvate and public instance attribute
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ returns the current square area """
+        return self.__size ** 2
