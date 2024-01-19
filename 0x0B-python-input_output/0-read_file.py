50a93535d205c0353480b210a6e9bc06cd9b7d73
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-01-19 03:38:04 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+A function that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """ reads a file and prints to stdout """
+    with open('my_file_0.txt', encoding="utf-8") as f:
+        print(f.read(), end="")
