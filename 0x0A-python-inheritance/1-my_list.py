64019209dc066855c38dfd238868da8cb083bfe4
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-05 11:49:45 -0800

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,4 +8,5 @@ A class MyList that inherits from list
 class MyList(list):
     """ prints the list but sorted """
     def print_sorted(self):
+        """ Prints sorted list """
         print(sorted(self))
