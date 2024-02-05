83d1ee4d1f19210be0dc55af31abdd78feaae149
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-05 13:35:27 -0800

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+""" Square """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ A class square that inheritis from Rectangle """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return super().area()
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
