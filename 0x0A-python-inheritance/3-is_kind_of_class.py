503521ae13dc44235994db747239a66b57ae81c9
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-05 12:11:17 -0800

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" A function that inherits from a class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Returns True or False """
+
+    return isinstance(obj, a_class)
