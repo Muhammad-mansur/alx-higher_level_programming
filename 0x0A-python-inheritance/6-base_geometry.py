ed2024fdd68b533839add305e69cbff77430560e
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-05 12:29:09 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Improve Geometry"""
+
+
+class BaseGeometry:
+    """ 
+    Raises:
+        Exception: area() is not implemented
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
