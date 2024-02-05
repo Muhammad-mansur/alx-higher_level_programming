809703bab21a100c0ddfeff062a2bf29d8d39b88
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-05 12:00:38 -0800

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" A function """
+
+
+def is_same_class(obj, a_class):
+    """ 
+    Returns:
+        True: if the object is exactly an intance of the specified class
+        False: Otherwise
+    """
+    if type(obj) is a_class:
+        return True
+
+    else:
+        return False
