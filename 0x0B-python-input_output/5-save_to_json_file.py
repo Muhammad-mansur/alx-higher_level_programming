3e8a28324058202404548c5c83e83351ebeb975e
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-06 12:35:21 -0800

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Save Object to a file """
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes an Object to a text file, using a JSON representation """
+    with open(filename, 'w') as myFile:
+        return json.dump(my_obj, myFile)
