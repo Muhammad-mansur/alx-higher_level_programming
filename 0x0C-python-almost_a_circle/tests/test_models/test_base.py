e85097745a146e936a9d2e92eb35eb6e45a96d5e
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-12 13:36:57 -0800

    base test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+""" Testing base """
+
+
+import unittest
+import base from Base
+
+
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        """ Create some instances for testing """
+        self.rectangle_instance = Base.create(width=5, height=10)
+        self.square_instance = Base.create(size=7)
+
+        """ Save instances to files """
+        Base.save_to_file([self.rectangle_instance, self.square_instance])
+
+    def test_load_from_file_nonempty(self):
+        """ Test loading from existing files """
+        instances = Bases.load_from_file()
+
+        """ Ensure correct number of instances loaded """
+        self.assertEqual(len(instances), 2)
+
+        """
+        Ensure instances are of correct types and have correct attributes
+        """
+        self.assertequalIsInstance(instances[0], type(self.rectangle_instance))
+        self.assertEqual(instances[0].width, 5)
+        self.assertEqual(instances[0].height, 10)
+        self.assertIsInstance(instances[1], type(self.square_instance))
+        self.assertEqual(instances[1].size, 7)
+
+    def test_create_rectangle(self):
+        """ Test creating a square instance """
+        square = Base.create(size=5)
+        self.assertEqual(square.size, 5)
+
+    def test_to_json_string(self):
+        """ Test conversion to JSON string """
+        dictionary = [{'width': 5, 'height': 10}, {'size': 7}]
+        json_string = Base.to_json_string(dictionary)
+        self.assertEqual(json_string, '[{"width": 5, "height": 10},
+        {"size": 7}]')
+
+    def test_from_json_string(self):
+        """ Test conversion from JSON string """
+        json_string = '[{"width": 5, "height": 10}, {"size": 7}]'
+        dictionary = Base.from_json_string(json_string)
+        self.assertEqual(dictionary, [{'width': 5, 'height': 10}, {'size': 7}])
+
+if __name__ == '__main__':
+    unittest.main()
