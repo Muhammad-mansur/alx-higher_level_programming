83bfe51a152fa2413326f970982a37c8ce52b0e7
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-12 11:23:11 -0800

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 """ Base class """
 
 
+import json
+
+
 class Base:
     """ private class attribute """
     __nb_objects = 0
@@ -14,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return []
+
+        else:
+            return json.dumps(list_dictionaries)
