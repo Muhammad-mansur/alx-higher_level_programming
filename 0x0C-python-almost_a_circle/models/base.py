52d82ac5cbc96a10510a4badf94800421f8b5cb0
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-02-12 11:46:14 -0800

    task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,19 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None:
-            return []
+            return "[]"
 
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+
+        filename = cls.__name__ + ".json"
+
+        with open(filename, 'w') as f:
+            json_string = cls.to_json_string([obj.to_dictionary() for obj in
+                    list_objs])
+            f.write(json_string)
