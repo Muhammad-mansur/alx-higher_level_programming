128bb4ca92bffd69832f4c022cf87d418b73298f
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-03-02 05:16:02 -0800

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" """
+
+
+def update_dictionary(a_dictionary, key, value):
+    new_value = ""
+
+    for i in a_dictionary:
+        if key == a_dictionary:
+            value = new_value
+        elif key not in a_dictionary:
+            value.append(new_value)
