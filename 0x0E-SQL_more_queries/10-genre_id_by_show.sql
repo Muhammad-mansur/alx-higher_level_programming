466007fb493f67081f6be5eb50bfe9236a758ee7
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-14 00:32:05 +0530

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,6 @@
 USE hbtn_0d_tvshows;
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+FROM tv_show_genres
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
