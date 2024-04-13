761303609c77fccd34ed9a222a78069b14214020
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-14 00:39:07 +0530

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server.
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, genre_id
 FROM tv_show_genres
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
