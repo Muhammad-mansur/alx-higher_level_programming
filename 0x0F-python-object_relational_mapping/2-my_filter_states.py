74e95da39c6bc8bf4ea3c340510520d3ff4f98e0
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-19 23:47:37 +0530

    2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Filter states by user input"""
+
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states\
+        WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
