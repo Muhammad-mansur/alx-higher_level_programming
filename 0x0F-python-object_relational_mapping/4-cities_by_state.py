3c190d8d4f20ba6b9fd098d199f8331c101bb5eb
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-20 00:47:18 +0530

    cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     cur = conn.cursor()
 
-    query = "SELECT cities.id, cities.anme, states.name FROM cities\
+    query = "SELECT cities.id, cities.name, states.name FROM cities\
         JOIN states ON cities.state_id = states.id\
         ORDER BY cities.id ASC"
 
