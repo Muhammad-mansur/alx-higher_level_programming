84c61fb8172cddf801d00e82175ddf10f7124d59
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-20 00:51:02 +0530

    cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     import MySQLdb
 
     conn = MySQLdb.connect(
-        host='localhost'
+        host='localhost',
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
