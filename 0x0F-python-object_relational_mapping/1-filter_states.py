fa4d711487d9473909abbbdc04b8f0c03f203515
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-19 23:23:46 +0530

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 
-"""0. Get all states"""
+"""1. Filter states"""
 
 
 if __name__ == '__main__':
-
+    
     import MySQLdb
     import sys
-
+    
     # connect to MySQL server
     conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            charset="utf8")
-
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    
     # create a cursor object
     cur = conn.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
+    
+    cur.execute(
+        "SELECT * FROM statesWHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    
     # fetch all rows
     rows = cur.fetchall()
-
+    
     # iterate through each row and display results
     for row in rows:
         print(row)
-
+        
     # close the cursor
-    cur.close()
+    cur.close
     
     # close server connection
-    conn.close()
+    conn.close
