3af93dffb6fa7600dad2e7f0a44f99ee2e1d129e
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-19 23:31:21 +0530

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     # create a cursor object
     cur = conn.cursor()
 
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
     # fetch all rows
     rows = cur.fetchall()
