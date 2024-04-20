97df72bcb70774c78308ccb30fbc2db439a0b952
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-21 02:13:27 +0530

    cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,17 +15,19 @@ if __name__ == '__main__':
         passwd=sys.argv[2],
         db=sys.argv[3],
     )
-    
+
     cur = conn.cursor()
-    
-    cur.execute("SELECT cities.name FROM cities\
+
+    que = "SELECT cities.name FROM cities\
         JOIN states ON cities.state_id = states.id\
         WHERE states.name = %s\
-        ORDER BY cities.id ASC")
-    
+        ORDER BY cities.id ASC"
+
+    cur.execute(que, [sys.argv[4]])
+
     query_rows = cur.fetchall()
-    
+
     print(', '.join(row[0] for row in query_rows))
-    
+
     cur.close()
     conn.close()
