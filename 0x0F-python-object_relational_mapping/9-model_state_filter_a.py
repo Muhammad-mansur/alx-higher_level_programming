e9a9c1d0465da1b7e9e507a73807bd14cf3d7328
Muhammad Mansur <muhammadmansur737@gmail.com>
2024-04-21 04:27:18 +0530

    filter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Filter"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    
+    # Create engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+    )
+    
+    # Create session
+    session = sessionmaker(bind=engine)
+    session = session()
+    
+    Base.metadata.create_all(engine)
+    
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
+    
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+
+    # Close session
+    session.close()
