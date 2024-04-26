1b170d4277dcdebb09c42aa58d311e78db2afa41
muhammad-mansur <Muhammadmansur737@gmail.com>
2024-04-26 14:51:20 -0700

    task8 fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,14 +9,15 @@ import requests
 if __name__ == "__main__":
 
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    
+
     # Send a POST request to the URL with the letter as a parameter
-    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
-    
+    response = requests.post("http://0.0.0.0:5000/search_user", data={
+        "q": letter})
+
     try:
         # Parse the JSON response
         data = response.json()
-        
+
         # Check if the JSON response is properly formatted and not empty
         if data:
             # Display the id and name
